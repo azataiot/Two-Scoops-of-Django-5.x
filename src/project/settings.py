633f3636fffe9ee894a4151d7ab4e39c248208ba
Azat <8280770+azataiot@users.noreply.github.com>
2024-03-11 12:01:35 +0300

    init new app core, in Example 6.1: core/models.py

--- src/project/settings.py
+++ src/project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "icecreamratings.apps.IcecreamratingsConfig"
+    "icecreamratings.apps.IcecreamratingsConfig",
+    "core.apps.CoreConfig"
 ]
 
 MIDDLEWARE = [
